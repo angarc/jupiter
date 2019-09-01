0cc22ace77470c8857f2af00f5dd2640eb6b8015
Angel Garcia <angelgarcia@dyn-160-39-226-14.dyn.columbia.edu>
2019-08-31 23:06:37 -0400

    Initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,13 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+require("trix")
+require("@rails/actiontext")
+
+import $ from 'jquery'
+import 'bootstrap'
+import 'startbootstrap-sb-admin-2/js/sb-admin-2'
+
+import '../src/stylesheets/application.scss'
