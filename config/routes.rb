ec1c6b038dfb01a3c6e6ca83b6e6e9b7f01e4396
Angel Garcia <angelgarcia@dyn-160-39-226-134.dyn.columbia.edu>
2019-09-06 17:54:01 -0400

    Added some styling to sidebar and prepped for dynamic info

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, {
+    controllers: {
+      registrations: 'users/registrations',
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      invitations: 'users/invitations'
+    }
+  }
   
   root 'dashboard#index'
 end
