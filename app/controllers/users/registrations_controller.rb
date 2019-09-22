7b6495e6b002a2388faf044317a3fb9cc74c3383
Angel Garcia <angelgarcia@dyn-160-39-238-51.dyn.columbia.edu>
2019-09-21 21:22:29 -0400

    Restricting registrations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   before_action :configure_permitted_parameters
 
+  def new
+    flash[:danger] = "You aren't authorized to perform that action"
+    redirect_to new_user_session_path
+  end
+
+  def create
+    flash[:danger] = "You aren't authorized to perform that action"
+    redirect_to new_user_session_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
