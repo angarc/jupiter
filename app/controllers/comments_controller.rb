75ea129152981ba01e514ff414c9964dec17296f
Angel Garcia <angelgarcia@dyn-160-39-235-179.dyn.columbia.edu>
2019-11-10 02:10:47 -0500

    Users can add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,25 @@
 class CommentsController < ApplicationController
 
-  
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = current_user.comments.build(comment_params) 
+    @comment.task_id = @task.id
+    if @comment.save
+      @comments = @task.comments 
+      respond_to do |format|
+        format.js
+      end 
+    else
+      respond_to do |format|
+        format.js
+      end 
+    end
+  end 
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end 
 
 end
