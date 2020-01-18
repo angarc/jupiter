6a6bc1e7ed0319ae91c1cdc769f82971b706f42b
Angel Garcia <angarc37@gmail.com>
2020-01-18 00:23:13 -0700

    Updating deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,10 +16,10 @@ set :keep_releases, 5
 # This is useful if you don't want to use ENV variables
 append :linked_files, 'config/master.key'
 
-after "deploy:assets:precompile", "deploy:bin_webpack"
+after "deploy:finished", "deploy:bin_webpack"
 
 namespace :deploy do
-  desc 'Run rake yarn:install'
+  desc 'Run rake bin_webpack'
   task :bin_webpack do
     on roles(:web) do
       within release_path do
