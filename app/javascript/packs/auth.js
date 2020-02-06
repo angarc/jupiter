bf7226df789bd8f60e265c06103288025f308257
Angel Garcia <angarc37@gmail.com>
2020-02-06 16:11:35 -0500

    removed some code from auth.js

--- app/javascript/packs/auth.js
+++ app/javascript/packs/auth.js
@@ -1,11 +1,3 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-
-import $ from 'jquery'
-window.$ = $
 import 'bootstrap'
 
 import '../src/stylesheets/auth.scss'
-
-// import toastr from 'toastr/toastr'
-// window.toastr = toastr
